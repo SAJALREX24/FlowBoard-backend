c43a341d6f26f14bb0bb294bb404a6476ac65569
SAREXER <sajalsaraswat12@gmail.com>
2026-04-22 06:29:30 +0530

    Auth service: full register/login flow with JWT and BCrypt, all endpoints tested

--- services/FlowBoard.Auth/Program.cs
+++ services/FlowBoard.Auth/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using FlowBoard.Auth.Data;
+using FlowBoard.Auth.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AuthDb")));
-
+builder.Services.AddScoped<IAuthService, AuthServiceImpl>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
